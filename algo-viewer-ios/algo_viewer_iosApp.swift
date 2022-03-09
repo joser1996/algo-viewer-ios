d3ea0385b22f4218931314317cbe584caa286941
Jose Torres <josertrs@gmail.com>
2022-03-09 11:58:57 -0800

    Initial Commit

---
+++ algo-viewer-ios/algo_viewer_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  algo_viewer_iosApp.swift
+//  algo-viewer-ios
+//
+//  Created by Jose Torres-Vargas on 3/9/22.
+//
+
+import SwiftUI
+
+@main
+struct algo_viewer_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
